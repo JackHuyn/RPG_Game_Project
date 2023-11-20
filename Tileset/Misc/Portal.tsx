820a265403f2578004732e640d441ae4035efc84
Jack Huynh <76965377+JackHuyn@users.noreply.github.com>
2023-11-20 19:35:22 -0400

    update ui and tileset

---
+++ Tileset/Misc/Portal.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Portal" tilewidth="16" tileheight="16" tilecount="4" columns="4">
+ <image source="../../../../MiniWorldSprites/Miscellaneous/Portal.png" width="64" height="16"/>
+</tileset>
